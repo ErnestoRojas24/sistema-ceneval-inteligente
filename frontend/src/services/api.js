dc6091fe872a2c5018dcdc957e7b5b51d8d512c5
ErnestoRojas24 <A010171452@my.uvm.edu.mx>
2026-05-17 23:23:06 -0600

    Resolviendo conflictos y forzando la version final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000'
+const BASE_URL = 'https://sistema-ceneval-inteligente.onrender.com'
 
 export async function iniciarExamen() {
   const res = await fetch(`${BASE_URL}/iniciar`)
